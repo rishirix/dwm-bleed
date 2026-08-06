3aa556c810e6e8f2af56ed495ac195c4e61e6797
Rishi <rishibhargawa47@gmail.com>
2026-08-06 15:57:50 +0530

    New: CenteredWindowName patch

--- config.def.h
+++ config.def.h
@@ -42,7 +42,7 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int decorhints  = 1;    /* 1 means respect decoration hints */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-static const int refreshrate = 180;  /* refresh rate (per second) for client move/resize */
+static const int refreshrate = 60;  /* refresh rate (per second) for client move/resize */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
